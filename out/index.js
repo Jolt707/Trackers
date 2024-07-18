b89eb183f522b9f7f534f9d6ffb86eccb8d33825
jolt707 <sta0085@bhs.vic.edu.au>
2024-07-18 12:30:46 +1000

    Did the thing

--- out/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-require("module-alias/register");
-const typedi_1 = require("typedi");
-const server_1 = require("./server");
-const server = typedi_1.Container.get(server_1.Server);
-server.init();
-//# sourceMappingURL=index.js.map
